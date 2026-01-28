1c40ffbcac106d7543915fe7b3805e8880ace697
rin-dz <dz.rina.na@gmail.com>
2026-01-28 12:36:56 +0300

    initialize the project

---
+++ src/components/App/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import AccessTimeIcon from '@mui/icons-material/AccessTime';
+
+function App() {
+  return (
+    <div className="App">
+    </div>
+  );
+}
+
+export default App;
